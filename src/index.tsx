8cb4d244da33461e5260ff38734aa6bc276be701
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-11-06 01:02:26 -0300

    init repo!

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { customTheme } from "./theme";
+import Global from "./theme/global";
+import ptBR from "./i18n/langs/ptBR.json";
+import { IntlProvider } from "react-intl";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const messages = ptBR;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <IntlProvider locale={"pt-BR"} defaultLocale={"pt-BR"} messages={messages}>
+    <ThemeProvider theme={customTheme}>
+      <App />
+      <Global />
+    </ThemeProvider>
+  </IntlProvider>
+);
